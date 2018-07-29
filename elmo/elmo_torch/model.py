e77840bc8a9fc7abfc4f93293ed52ddcdd1695da
yuriak <yuriak1911@gmail.com>
2018-07-29 23:21:49 +1000

    implemented elmo_torch

--- elmo/elmo_torch/model.py
+++ elmo/elmo_torch/model.py
@@ -47,7 +47,7 @@ class BiLM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, word_number, dropout):
         super(BiLM, self).__init__()
         self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True, batch_first=True)
         self.fw_fc = nn.Linear(hidden_size, word_number)
         self.bw_fc = nn.Linear(hidden_size, word_number)
         self.dropout = dropout
